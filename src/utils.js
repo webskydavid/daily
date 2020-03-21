21a93c8aa40e9b3bc419e436c5f703e751f78709
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2020-03-21 10:19:17 +0100

    Added formatting and switch days

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,23 @@
 import moment from "moment";
 
-export const formatCurrentDate = (format = "DDMMYYYY") => {
+const DEFAULT_FORMAT = "DDMMYYYY";
+
+export const formatCurrentDate = (format = DEFAULT_FORMAT) => {
   return moment().format(format);
 };
 
 export const timestamp = () => {
   return moment().valueOf();
+};
+
+export const nextDayFormatted = date => {
+  return moment(date, DEFAULT_FORMAT)
+    .add(1, "days")
+    .format(DEFAULT_FORMAT);
+};
+
+export const prevDayFormatted = date => {
+  return moment(date, DEFAULT_FORMAT)
+    .subtract(1, "days")
+    .format(DEFAULT_FORMAT);
 };
