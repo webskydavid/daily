27a4fe668edc1d1ed4128572ddff10848d243a06
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2020-03-24 09:20:31 +0100

    Added Head component

---
+++ src/components/Head.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Context, types } from "./../reducer";
+import { formatDate, formatCurrentDate } from "./../utils";
+import style from "./Head.module.scss";
+import Button from "./elements/Button";
+const Head = () => {
+  const {
+    state: { currentDay },
+    dispatch
+  } = React.useContext(Context);
+  return (
+    <div className={style.head}>
+      <Button
+        size="small"
+        text="Prev"
+        onClick={() => dispatch({ type: types.PREV_DAY })}
+      />
+
+      {formatDate(currentDay, "DDMMYYYY", "DD-MMMM-YYYY")}
+      {currentDay !== formatCurrentDate() && (
+        <Button
+          size="small"
+          text="Next"
+          onClick={() => dispatch({ type: types.NEXT_DAY })}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Head;
