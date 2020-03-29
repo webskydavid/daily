24ff7d439bebc7d643abb6da9e8d4801dbfc767b
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2020-03-29 13:28:46 +0200

    Added memo for optimizeing rendering

--- src/components/Day/Day.jsx
+++ src/components/Day/Day.jsx
@@ -8,39 +8,43 @@ const Day = ({ data }) => {
     dispatch
   } = React.useContext(Context);
 
-  return (
-    <div key={data.id}>
-      <h5>
-        {data.title} {data.time}{" "}
-        <Button
-          variant="info"
-          size="small"
-          text="Edit"
-          onClick={() =>
-            dispatch({
-              type: types.IS_EDIT,
-              payload: {
-                date: currentDay,
-                id: data.id
-              }
-            })
-          }
-        />
-        <Button
-          variant="danger"
-          size="small"
-          text="Delete"
-          onClick={() =>
-            dispatch({
-              type: types.REMOVE,
-              payload: { id: data.id }
-            })
-          }
-        />
-      </h5>
-      <p>{data.content}</p>
-    </div>
-  );
+  const day = React.useMemo(() => {
+    return (
+      <div key={data.id}>
+        <h5>
+          {data.title} {data.time}{" "}
+          <Button
+            variant="info"
+            size="small"
+            text="Edit"
+            onClick={() =>
+              dispatch({
+                type: types.IS_EDIT,
+                payload: {
+                  date: currentDay,
+                  id: data.id
+                }
+              })
+            }
+          />
+          <Button
+            variant="danger"
+            size="small"
+            text="Delete"
+            onClick={() =>
+              dispatch({
+                type: types.REMOVE,
+                payload: { id: data.id }
+              })
+            }
+          />
+        </h5>
+        <p>{data.content}</p>
+      </div>
+    );
+  }, [currentDay, data.content, data.id, data.time, data.title, dispatch]);
+
+  return day;
 };
 
 export default Day;
